20435efb180b7fcc798d0ea71f10e716a99a2c08
Norbu Lama <norbuhyolmo7@gmail.com>
2021-10-23 19:33:52 +0545

    changed

--- controller/category-controller.js
+++ controller/category-controller.js
@@ -1,25 +1,58 @@
-const { request, response } = require("express");
 
 let category = [];
 
-const getAllCategory = (request, response) => {
+
+exports.getAllCategory = (request,response)=>{
     response.json(category);
 }
+// const getAllCategory = (request, response) => {
+//     response.json(category);
+// }
 
-const store = (request, response) => {
+exports.store = (request, response) => {
     category.push(request.body);
     response.status(201).json(request.body);
 }
 
-const getById = (request, response) => {
-    let category = category.find((user) => category.id === parseInt(request.params.id));
-    if (!category) response.json(404).send 
-    "The category with the given ID was not found."
-    response.json(category);
+// const store = (request, response) => {
+//     category.push(request.body);
+//     response.status(201).json(request.body);
+// }
+
+exports.getById = (request, response) => {
+    let userIndex = category.find(user => user.id === parseInt(request.params.id));
+     if (!userIndex) {
+         return response.json({
+             error : "the given category id was not found"
+         })
+     }
+     response.json(userIndex);
 }
 
-module.export = {
-    getAllCategory,
-    store, 
-    getById,
+exports.updateCategory = (request, response) => {
+    let userIndex = category.findIndex(cat=> cat.id === parseInt(request.params.id));
+    if (userIndex === -1) {
+        return response.status(404).send({
+            error : "The given ID was not found"
+        })
+    }
+    category[userIndex]["id"] = request.body.id;
+    category[userIndex]["title"] = request.body.title;
+    category[userIndex]["createdDate"] = request.body.createdDate;
+    response.json(request.body);
 }
+
+exports.deleteCategory = (request, response) => {
+    let userIndex = category.findIndex(cat => cat.id === parseInt(request.params.id));
+    if (userIndex === -1) {
+        return response.json({
+            error : "The given ID was not found"
+        })
+    }
+    category.splice(userIndex, 1);
+    response.status(204).json();
+}
+// module.export = {
+//     store, 
+//     getById,
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,24 @@
 const { request, response } = require("express");
 const express = require("express");
 const router = express.Router();
-const CategoryController = require("../controller/category-controller")
+const categoryController = require("../controller/category-controller")
 
 let category = [];  // { ID, Title, CreatedDate}
 
 // GET ALL CATEGORY, REQUEST METHOD = GET
-router.get('/category', (request, response) => {
-    response.json(category);
-})
+router.get('/category', categoryController.getAllCategory);
 
 // CREATE NEW CATEGORY, REQUEST METHOD = POST
-router.post('/category', (request, response) => {
-    category.push(request.body);
-    response.status(201).json(request.body);
-})
+router.post('/category',categoryController.store);
 
 // GET CATEGORY BY ID, REQUEST METHOD = GET
-router.get('/category/:id', (request, response) => {
-    let userIndex = category.find(user => user.id === parseInt(request.params.id));
-     if (!userIndex) 
-          return response.status(404).send
-    ({
-        error: "The given ID was not found"
-    })
-    response.json(userIndex)
-    // response.json
-    // ({
-    //     id: request.params.id 
-    // });
-})
+router.get('/category/:id',categoryController.getById);
 
 // UPDATE CATEGORY BY ID, REQUEST METHOD = PUT
-router.put('/category/:id', (request, response) => {
-    let userIndex = category.findIndex(cat => category.id === parseInt(request.params.id));
-    if (!userIndex === -1) {
-        return response.status(404).send
-        ({
-            error : "The given ID was not found"
-        })
-    }
-    users[userIndex] ["id"] = request.body.id;
-    users[userIndex] ["title"] = request.body.title;
-    users[userIndex] ["createdDate"] = request.body.createdDate;
-    response.json(request.body);
-})
+router.put('/category/:id', categoryController.updateCategory);
 
 // DELETE CATEGORY BY ID, REQUEST METHOD = DELETE
-router.delete('/category/:id', (request, response) => {
-    let userIndex = category.findIndex((cat) => user.id === parseInt(request.params.id));
-    if (userIndex === -1) {
-        return response.json
-        ({
-            error : "The given ID was not found"
-        })
-    }
-    users.splice(userIndex, 1);
-    response.json(request.body);
-})
+router.delete('/category/:id', categoryController.deleteCategory);
 
 
 module.exports = router
